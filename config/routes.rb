efa3259e929da8d96e8c41d3e74234b391e687e5
Sloba <my@email.com>
2011-06-07 04:56:12 -0700

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-
+  
+  resources :users
+  
   match '/about', :to => 'pages#about'
   match '/contact', :to => 'pages#contact'
   match '/help', :to => 'pages#help'
